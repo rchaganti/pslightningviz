85605df86dd1b2c742734371cee4aaefd9f34fda
Ravikanth C <ravikanth@ravichaganti.com>
2017-07-21 12:40:36 +0530

    POC

---
+++ pslightningviz.psm1
@@ -0,0 +1,76 @@
+function New-PSLightningVizSession
+{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $LightningHost = 'http://public.lightning-viz.org',
+
+        [Parameter()]
+        [String]
+        $SessionName = 'PSLVizSession'
+    )
+
+    $sessionUrl = "${LightningHost}/sessions/"
+    
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add('Content-type', 'application/json')
+    $headers.Add('Accept', 'text/plain')
+
+    $json = @{
+        name = $SessionName
+    } | ConvertTo-Json
+
+    Invoke-RestMethod $sessionUrl -Method POST -Body $json -Headers $headers
+}
+
+function New-PSLightningVizVisualization
+{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [PSCustomObject]
+        $Session,
+
+        [Parameter()]
+        [String]
+        $Type,
+
+        [Parameter()]
+        [Object]
+        $Data,
+
+        [Parameter()]
+        [Switch]
+        $Save,
+
+        [Parameter()]
+        [String]
+        $ImagePath,
+
+        [Parameter()]
+        [Switch]
+        $OpenInBrowser
+    )
+
+    $visualizationUrl = "http://public.lightning-viz.org/sessions/$($Session.id)/visualizations/"
+    $json = @{
+        type = $Type
+        data = $Data
+    } | ConvertTo-Json
+
+    $response = Invoke-RestMethod $visualizationUrl -Method POST -Body $json -Headers $headers
+
+    if ($Save)
+    {
+        Invoke-WebRequest -Uri "http://public.lightning-viz.org/visualizations/$($response.id)/screenshot/?width=1024&height=768" -OutFile $ImagePath
+    }
+    elseif ($OpenInBrowser)
+    {
+        Start-Process "http://public.lightning-viz.org/visualizations/$($response.id)/screenshot/?width=1024&height=768"
+    }
+    else
+    {
+        return $response
+    }
+}
